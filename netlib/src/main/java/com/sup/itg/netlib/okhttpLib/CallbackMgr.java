8ff659b5bd4b115c12f6fad586eb37197f7960fd
luozhiran <820185535@qq.com>
2019-06-27 14:41:37 +0800

    加入双监听器

--- netlib/src/main/java/com/sup/itg/netlib/okhttpLib/CallbackMgr.java
+++ netlib/src/main/java/com/sup/itg/netlib/okhttpLib/CallbackMgr.java
@@ -3,6 +3,8 @@ package com.sup.itg.netlib.okhttpLib;
 import com.sup.itg.netlib.okhttpLib.interfaces.ItgProgressback;
 import com.sup.itg.netlib.okhttpLib.interfaces.ItgTask;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -10,6 +12,8 @@ public class CallbackMgr {
 
     private Object mLock = new Object();
     private Map<String, ItgProgressback> itgProgressbackMap;
+
+    private List<ItgProgressback> mItgProgressbacks;
 
     public CallbackMgr() {
 
@@ -40,11 +44,38 @@ public class CallbackMgr {
     }
 
 
+    public void addItgProgress1(ItgProgressback progressback) {
+        if (mItgProgressbacks == null) {
+            mItgProgressbacks = new ArrayList<>();
+        }
+        synchronized (mLock) {
+            mItgProgressbacks.add(progressback);
+        }
+    }
+
+    public void removeItgProgress1(ItgProgressback progressback) {
+        synchronized (mLock) {
+            if (mItgProgressbacks == null) {
+                mItgProgressbacks.remove(progressback);
+            }
+        }
+
+    }
+
+
     public void loop(ItgTask task) {
         if (itgProgressbackMap != null) {
             ItgProgressback itgProgressback = itgProgressbackMap.get(task.url());
             if (itgProgressback != null) {
                 itgProgressback.itgProgress(task);
+            }
+        }
+
+        synchronized (mLock) {
+            if (mItgProgressbacks != null) {
+                for (ItgProgressback itgProgressback : mItgProgressbacks) {
+                    itgProgressback.itgProgress(task);
+                }
             }
         }
     }
@@ -54,6 +85,14 @@ public class CallbackMgr {
             ItgProgressback itgProgressback = itgProgressbackMap.get(url);
             if (itgProgressback != null) {
                 itgProgressback.fail(msg, url);
+            }
+        }
+
+        synchronized (mLock) {
+            if (mItgProgressbacks != null) {
+                for (ItgProgressback itgProgressback : mItgProgressbacks) {
+                    itgProgressback.fail(msg, url);
+                }
             }
         }
     }
