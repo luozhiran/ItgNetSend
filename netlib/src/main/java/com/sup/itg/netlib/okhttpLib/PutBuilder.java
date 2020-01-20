77cd3cf4b17191b732040306c8905657fc0b9626
zhangyongcan <volcano.dayarch@gmail.com>
2020-01-20 15:55:37 +0800

    加入putBuilder

---
+++ netlib/src/main/java/com/sup/itg/netlib/okhttpLib/PutBuilder.java
@@ -0,0 +1,26 @@
+package com.sup.itg.netlib.okhttpLib;
+
+import okhttp3.Call;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class PutBuilder extends AdapterBuilder {
+    public PutBuilder(OkHttpClient okHttpClient) {
+        super(okHttpClient);
+    }
+
+    @Override
+    public Call createCall() {
+        Request.Builder builder = new Request.Builder();
+        Headers headers = getHeader();
+        if (headers != null) {
+            builder.headers(headers);
+        }
+        builder.tag(mUrl);
+        builder.url(mUrl);
+        builder.put(getRequestBody());
+        Call call = mOkHttpClient.newCall(builder.build());
+        return call;
+    }
+}
